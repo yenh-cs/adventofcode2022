73a37b7cbc47699d9d04914cbbc0000aebe38b8a
yenh-cs <yen.hoang@contentsquare.com>
2022-12-01 15:55:29 +0100

    Day1

---
+++ day1/puzzle2.py
@@ -0,0 +1,30 @@
+
+def totalCaloriesOfEachElve(data):
+    totalCaloriesPerElve = []
+    i = 0
+    calories = 0
+
+    while i < len(data):
+        if data[i] == '\n':
+            totalCaloriesPerElve.append(calories)
+            calories = 0
+            i += 1
+            continue
+        data[i] = int(data[i].strip())
+        calories += data[i] 
+        i += 1 
+
+    totalCaloriesPerElve.append(calories)
+
+    return totalCaloriesPerElve
+
+
+def main():
+    with open('input.txt') as f:
+        data = f.readlines()
+    totalCaloriesPerElve = totalCaloriesOfEachElve(data)
+    sumOfTopThree = sum(sorted(totalCaloriesPerElve)[-3:])
+    print(sumOfTopThree)
+
+if __name__ == '__main__':
+    main()
